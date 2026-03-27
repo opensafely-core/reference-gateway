7f345da29f04704ed96d6d7c7a884c351cbdb954
bloodearnest <bloodearnest@gmail.com>
2026-03-27 14:21:00 +0000

    Add some debugging info
     - request log
     - gunicorn access and error logs
     - alternate /healthz check url to not block deploys

--- gateway/settings.py
+++ gateway/settings.py
@@ -139,6 +139,23 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django.request": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
+
 
 # Per-deployment configuration
 
